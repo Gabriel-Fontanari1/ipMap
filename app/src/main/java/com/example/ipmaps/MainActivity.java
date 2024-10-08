486d6266c8f98e035d7f0fe9b074ad41b02ffcef
imply <gabrielfontanari1@gmail.com>
2024-10-08 09:39:22 -0300

    inicio app

---
+++ app/src/main/java/com/example/ipmaps/MainActivity.java
@@ -0,0 +1,67 @@
+package com.example.ipmaps;
+
+import android.os.Bundle;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback, GoogleMap.OnMapClickListener, GoogleMap.OnMapLongClickListener {
+
+    EditText textLatitude, textLongitude;
+    GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        textLatitude = findViewById(R.id.textLatitude);
+        textLongitude = findViewById(R.id.textLongitude);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        mMap = googleMap;
+        this.mMap.setOnMapClickListener(this);
+        this.mMap.setOnMapLongClickListener(this);
+
+        LatLng santaCruz = new LatLng(-29.6890566,-52.4558563);
+        mMap.addMarker(new MarkerOptions().position(santaCruz).title("Santa Cruz"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(santaCruz));
+    }
+
+    @Override
+    public void onMapClick(@NonNull LatLng latLng) {
+        textLatitude.setText(""+latLng.latitude);
+        textLongitude.setText(""+latLng.longitude);
+
+    }
+
+    @Override
+    public void onMapLongClick(@NonNull LatLng latLng) {
+        textLatitude.setText(""+latLng.latitude);
+        textLongitude.setText(""+latLng.longitude);
+
+    }
+}
